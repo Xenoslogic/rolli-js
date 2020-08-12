1793968e942edfd9ab344ee6151ed7fb9a8959b3
Dmitriy <sdv666999@gmail.com>
2020-08-12 13:25:09 +0300

    Add files via upload
    добавили добавление в корзину. БЕЗ подсчета суммы и возможности убирать из корзины.

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ buttons.forEach(function(item){
 		let counter = counterWrapper.querySelector('[data-counter]');
 
 // Для плюса - счетчик инкрементируем, для минуса - декрементируем (проверяем >1)
-
-		if (this.dataset.action === 'plus'){
+			if (this.dataset.action === 'plus'){
 			
 			counter.innerText = ++counter.innerText;
 		} else {
@@ -22,6 +21,65 @@ buttons.forEach(function(item){
 		}
 	})
 })
+
+// Находим все кнопки добавить в корзину по data аттрибуту
+let cartButtons = document.querySelectorAll('[data-cart]');
+
+// Отслеживаем клик на кнопке в корзину
+cartButtons.forEach(function(item){
+	item.addEventListener('click', function(){
+		// Определяем внутри какой карточки кликнули
+		let currentCard = this.closest('.card');
+
+		// собираем данные с этой карточки
+		let imgSrc = currentCard.querySelector('.product-img').getAttribute('src');
+		let title = currentCard.querySelector('.item-title').innerText;
+		let pcsInBox = currentCard.querySelector('.text-muted').innerText;
+		let counter = currentCard.querySelector('.items__current').innerText;
+		let weight = currentCard.querySelector('.price__weight').innerText;
+		let price = currentCard.querySelector('.price__currency').innerText;
+		
+		// собранные данные подставляем в шаблон для товара в корзине
+		let cartItemHTML = `
+							<div class="cart-item">
+								<div class="cart-item__top">
+									<div class="cart-item__img">
+										<img src="${imgSrc}" alt="">
+									</div>
+									<div class="cart-item__desc">
+										<div class="cart-item__title">${title}</div>
+										<div class="cart-item__weight">${pcsInBox} / ${weight}</div>
+
+										
+										<div class="cart-item__details">
+
+											<div class="items items--small">
+												<div class="items__control" data-action="minus">-</div>
+												<div class="items__current" data-counter>${counter}</div>
+												<div class="items__control" data-action="plus">+</div>
+											</div>
+
+											<div class="price">
+												<div class="price__currency">${price}</div>
+											</div>
+
+										</div>
+										
+
+									</div>
+								</div>
+							</div>
+							`;
+		// находим в разметке место (.cart-wrapper) для добавления полученного
+		let cartWrapper = document.querySelector('.cart-wrapper');						
+		cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+		document.querySelector('.alert').classList.add('none');
+
+
+	});
+
+})
+
 
 
 
